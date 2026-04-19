e137a4a49feb102fad7cee129e72537245688b3d
Walmyr <wlsf82@gmail.com>
2026-04-19 19:48:24 +0200

    Implement the captcha button states scenario

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -127,14 +127,6 @@ describe("Cypress Simulator", () => {
       .and("be.visible")
    })
 
-  it("Captcha button states", () => {
-    
-  })
-
-  it("Captcha error", () => {
-    
-  })
-
   it("Run button - enabled/disabled states", () => {
     
   })
@@ -185,4 +177,27 @@ describe("Cypress Simulator - Cookies Consent", () => {
       .its("localStorage.cookieConsent")
       .should("be.equal", "declined")
    })
+})
+
+describe("Cypress Simulator - Captcha", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html")
+    cy.contains("button", "Login").click()
+  })
+
+  it("disables the captcha verify button when no answer is provided or it's cleared", () => {
+    cy.contains("button", "Verify").should("be.disabled")
+
+    cy.get("input[placeholder='Enter your answer']").type("1")
+
+    cy.contains("button", "Verify").should("be.enabled")
+
+    cy.get("input[placeholder='Enter your answer']").clear()
+
+    cy.contains("button", "Verify").should("be.disabled")
+  })
+
+  it("Captcha error", () => {
+    
+  })
 })
