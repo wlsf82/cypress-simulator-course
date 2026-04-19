dfb91aa5a312789a14c6d32a81e796fb6d5abcf8
Walmyr <wlsf82@gmail.com>
2026-04-19 19:31:33 +0200

    Create a test plan

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -1,5 +1,73 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("Cypress Simulator", () => {
+  it("success", () => {
+    
+   })
+
+  it("error: invalid command scenario", () => {
+    
+  })
+
+  it("warning", () => {
+    
+  })
+
+  it("error: valid comand without parentheses", () => {
+    
+  })
+
+  it("help", () => {
+    
+  })
+
+  it("maximize/minimize", () => {
+    
+  })
+
+  it("logout", () => {
+    
+  })
+
+  it("show and hide logout button", () => {
+    
+  })
+
+  it("Running... state", () => {
+    
+  })
+
+  it("Accept cookies", () => {
+    
+  })
+
+  it("Decline cookies", () => {
+    
+  })
+
+  it("Captcha button states", () => {
+    
+  })
+
+  it("Captcha error", () => {
+    
+  })
+
+  it("Run button - enabled/disabled states", () => {
+    
+  })
+
+  it("Reset textarea on logout and login", () => {
+    
+  })
+
+  it("Disable run button on logout and login", () => {
+    
+  })
+
+  it("Reset output on logout and login", () => {
+    
+  })
+
+  it("No cookings banner on the login page", () => {
+    
   })
 })
