6cea81d158a5a01caed5bcccb61af0a907268efa
Walmyr <wlsf82@gmail.com>
2026-04-19 19:39:35 +0200

    Implement the maximize/minimize scenario

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -68,9 +68,23 @@ describe("Cypress Simulator", () => {
       .and("be.visible")
    })
 
-  it("maximize/minimize", () => {
-    
-  })
+  it("maximizes and minimizes a simulation result", () => {
+    cy.get("textarea[placeholder='Write your Cypress code here...']")
+      .type("cy.log('Yay!')")
+    cy.contains("button", "Run").click()
+
+    cy.get('.expand-collapse').click()
+
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "Success:")
+      .and("contain", "cy.log('Yay!') // Logged message 'Yay!'")
+      .and("be.visible")
+    cy.get("#collapseIcon").should("be.visible")
+
+    cy.get(".expand-collapse").click()
+
+    cy.get("#expandIcon").should("be.visible")
+   })
 
   it("logout", () => {
     
