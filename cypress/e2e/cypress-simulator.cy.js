6e31b6db4e958db06d8653124f124ee412b01b0c
Walmyr <wlsf82@gmail.com>
2026-04-19 19:44:04 +0200

    Implement the running state scenario

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -102,9 +102,30 @@ describe("Cypress Simulator", () => {
     cy.contains("button", "Logout").should("not.be.visible")
    })
 
-  it("Running... state", () => {
-    
-  })
+  it("shows the running state before showing the final result", () => {
+    cy.get("textarea[placeholder='Write your Cypress code here...']")
+      .type("cy.log('Yay!')")
+    cy.contains("button", "Run").click()
+
+    cy.contains("button", "Running...")
+      .should("be.disabled")
+      .and("be.visible")
+    cy.contains("#outputArea", "Running... Please wait.")
+      .should("be.visible")
+
+    cy.contains(
+      "button",
+      "Running...",
+      { timeout:  6000 }
+    ).should("not.exist")
+    cy.contains("button", "Run").should("be.visible")
+    cy.contains("#outputArea", "Running... Please wait.", { timeout: 6000})
+      .should("not.exist")
+    cy.get("#outputArea")
+      .should("contain", "Success:")
+      .and("contain", "cy.log('Yay!') // Logged message 'Yay!'")
+      .and("be.visible")
+   })
 
   it("Accept cookies", () => {
     
