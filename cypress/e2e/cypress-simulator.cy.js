ad8d0147e6e087f3c04f5d5f2c3db3d2bc8b4f9c
Walmyr <wlsf82@gmail.com>
2026-04-19 19:41:12 +0200

    Implement the show/hide logout button scenario

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -94,9 +94,13 @@ describe("Cypress Simulator", () => {
     cy.get("#sandwich-menu").should("not.be.visible")
    })
 
-  it("show and hide logout button", () => {
-    
-  })
+  it("shows and hides the logout button", () => {
+    cy.get("#sandwich-menu").click()
+    cy.contains("button", "Logout").should("be.visible")
+
+    cy.get("#sandwich-menu").click()
+    cy.contains("button", "Logout").should("not.be.visible")
+   })
 
   it("Running... state", () => {
     
