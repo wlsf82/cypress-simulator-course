d4273350d8ca4fac6c6e95086af7a10a9b8c400c
Walmyr <wlsf82@gmail.com>
2026-04-19 19:36:04 +0200

    Implement the warning test scenario

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -30,9 +30,16 @@ describe("Cypress Simulator", () => {
       .and("be.visible")
    })
 
-  it("warning", () => {
-    
-  })
+  it("shows a warning when entering and running a not-implemented Cypress command (e.g., cy.contains('Login'))", () => {
+    cy.get("textarea[placeholder='Write your Cypress code here...']")
+      .type("cy.contains('Login')")
+    cy.contains("button", "Run").click()
+
+    cy.get("#outputArea", { timeout: 6000})
+      .should("contain", "Warning:")
+      .and("contain", "The `cy.contains` command has not been implemented yet.")
+      .and("be.visible")
+   })
 
   it("error: valid comand without parentheses", () => {
     
