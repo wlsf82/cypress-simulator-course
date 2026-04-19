f56d7334b853984d5c2503b4ccd938e663ac7957
Walmyr <wlsf82@gmail.com>
2026-04-19 21:33:09 +0200

    Implement a test for the glitch in the Matrix scenario

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -95,3 +95,22 @@ describe("Cypress Simulator - Cookies Consent", () => {
       .should("be.equal", "declined")
    })
 })
+
+describe("Glitch in the Matrix", () => {
+  beforeEach(() => {
+    cy.login()
+    cy.visit("./src/index.html?skipCaptcha=true&chancesOfError=1", {
+      onBeforeLoad(win) {
+        win.localStorage.setItem("cookieConsent", "accepted")
+      }
+    })
+  })
+
+  it("errors out with a glitch in the Matrix", () => {
+    cy.run("cy.visit('http://example.com')")
+
+    cy.get("#outputArea", { timeout: 6000 })
+      .should("contain", "There's a glitch in the Matrix.")
+      .and("be.visible")
+  })
+})
