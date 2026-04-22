3afca390672dbdb34f4754e87ee439f3eb21cafa
Walmyr <wlsf82@gmail.com>
2026-04-22 23:41:56 +0200

    Fix wrongly implemented test

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -53,6 +53,17 @@ describe("Cypress Simulator", () => {
     cy.contains("button", "Run").should("be.disabled")
    })
 
+   it("disables the run button when logging off then logging in again", () => {
+    cy.get("textarea[placeholder='Write your Cypress code here...']")
+      .type("cy.log('Yo!')")
+
+    cy.get("#sandwich-menu").click()
+    cy.contains("button", "Logout").click()
+    cy.contains("button", "Login").click()
+
+    cy.contains("button", "Run").should("be.disabled")
+   })
+
   it("clears the code output when logging off then logging in again", () => {
     cy.run("cy.log('Yo!')")
 
